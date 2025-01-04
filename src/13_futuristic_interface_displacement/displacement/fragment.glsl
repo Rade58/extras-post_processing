394eedc0175c9068b156c898864afa4c6962551a
rade <bajic.rade2@gmail.com>
2025-01-04 06:40:58 +0100

    displacement pass interface texture

--- src/13_futuristic_interface_displacement/displacement/fragment.glsl
+++ src/13_futuristic_interface_displacement/displacement/fragment.glsl
@@ -27,11 +27,27 @@ void main() {
   // gl_FragColor = mytexture;
   // ------------------------------------------------------------
   
+  // it looks cool
+  // vec3 normalColor = texture2D(uNormalMap, vUv).xyz * 2.0 - 1.0;
+  // vec2 newUv = vUv + normalColor.xy * 0.1;
+  // vec4 color = texture2D(tDiffuse, newUv);
+  // 
+  // gl_FragColor = color;
+  // ------------------------------------------------------------
+  // improved
+  
   vec3 normalColor = texture2D(uNormalMap, vUv).xyz * 2.0 - 1.0;
   vec2 newUv = vUv + normalColor.xy * 0.1;
   vec4 color = texture2D(tDiffuse, newUv);
   
+  vec3 lightingDirection = normalize(vec3(-1.0, 1.0, 0.0));
+  float lightness = clamp(dot(normalColor, lightingDirection), 0.0, 1.0);
+  color += lightness * 2.0;
+
+
   gl_FragColor = color;
+
+
 
   // gl_FragColor = vec4(vec3(strength, strength, strength), 1.0);
 }
